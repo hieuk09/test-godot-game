ef1c4a6dfad4b6f7fd4517ee94b24366bcd9816f
Hieu Nguyen <hieuk09@gmail.com>
2018-02-18 18:47:39 +0700

    [f] Draw the main map

--- main.gd
+++ main.gd
@@ -3,7 +3,19 @@ extends Node
 # class member variables go here, for example:
 # var array = []
 
+const MAX_SIZE = 10
+
 func _draw():
-	var vector = Vector2(0, 0);
-	var c = Color(0.2, 1.0, .7, .8)
-	draw_circle(vector, 10, c);
+	var scale = 2
+	var i = 0
+	var size = Vector2(scale, scale)
+	var color = Color(1, 1, 0, 0.5)
+	while i < MAX_SIZE:
+		i += 1
+		var j = 0
+		while j < MAX_SIZE:
+			j += 1
+			var start = Vector2(i * scale, j * scale)
+
+			var rect = Rect2(start, size)
+			draw_rect(rect, color, false)
