38263a25d007aebcf3d7d9920d6af3c3962e31a1
Hieu Nguyen <hieuk09@gmail.com>
2018-02-21 01:27:53 +0700

    [f] Make movement work

--- player.gd
+++ player.gd
@@ -1,21 +1,22 @@
 extends KinematicBody2D
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+# This is a demo showing how KinematicBody2D
+# move_and_slide works.
 
-const MOTION_SPEED = 160
+# Member variables
+const MOTION_SPEED = 160 # Pixels/second
+
 
 func _physics_process(delta):
 	var motion = Vector2()
 	
-	if Input.is_action_pressed("move_up"):
+	if Input.is_action_pressed("ui_up"):
 		motion += Vector2(0, -1)
-	if Input.is_action_pressed("move_bottom"):
+	if Input.is_action_pressed("ui_down"):
 		motion += Vector2(0, 1)
-	if Input.is_action_pressed("move_left"):
+	if Input.is_action_pressed("ui_left"):
 		motion += Vector2(-1, 0)
-	if Input.is_action_pressed("move_right"):
+	if Input.is_action_pressed("ui_right"):
 		motion += Vector2(1, 0)
 	
 	motion = motion.normalized() * MOTION_SPEED
